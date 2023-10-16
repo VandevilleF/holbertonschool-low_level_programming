9f2d8507b2d10638906c40abd5aa1bc3084f84b7
VandevilleF <flo.vandeville@gmail.com>
2023-10-16 03:13:14 -0700

    Task 0 C - Variables, if, else, while

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Positive anything is better than negative nothing
+ * if: display is positive when n<0
+ * if else display is zero when n=0
+ * if else display is negative n>0
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
