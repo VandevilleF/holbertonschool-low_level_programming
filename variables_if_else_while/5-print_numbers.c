2a6e375da8ab16197803dba7171ac35ebb2f8d38
VandevilleF <flo.vandeville@gmail.com>
2023-10-16 08:03:37 -0700

    Task 5 C - Variables, is, else, while

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - void
+ * char: début de la fonction
+ * while: fin de la fonction
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0';i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
