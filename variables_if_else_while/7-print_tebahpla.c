804e9acd2821afb488d4d1165a8afb6dd2ff7dee
VandevilleF <flo.vandeville@gmail.com>
2023-10-16 08:26:41 -0700

    Task 7 C - Variables, if, else, while

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - void
+ * char: début de la fonction
+ * while: fin de la fonction
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
