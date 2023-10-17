3ccd5523aacf026d8a9de845be2c1fa7470160c1
VandevilleF <flo.vandeville@gmail.com>
2023-10-17 06:48:01 -0700

    Task 3 C - Fonctions, nested loops

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _islower: c
+ * if: si
+ * else: donc
+ * return: 0
+ */
+int _islower(int c)
+{
+	{
+
+	if (c >= 97 && c <= 122)
+	return (1);
+	else
+	return (0);
+	}
+	return (0);
+	}
