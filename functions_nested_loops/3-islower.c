78f86ffb25d12a472fea6f1992a6d4d9f3cec212
VandevilleF <flo.vandeville@gmail.com>
2023-10-17 07:02:01 -0700

    Update Task 3 modif pour betty#1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,13 @@
 #include "main.h"
 /**
- * int _islower: c
- * if: si
- * else: donc
- * return: 0
+ * _islower - lowercase
+ * @c: variable
+ * Return: 0
  */
 int _islower(int c)
 {
-	{
-
 	if (c >= 97 && c <= 122)
-	return (1);
+		return (1);
 	else
-	return (0);
-	}
-	return (0);
-	}
+		return (0);
+}
