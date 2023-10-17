32f65a3a42965b354333db069c67b008ebb1948d
VandevilleF <flo.vandeville@gmail.com>
2023-10-17 03:35:22 -0700

    Task 1 C - Fonctions, nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - void
+ * putchar: affiche un caractere
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
