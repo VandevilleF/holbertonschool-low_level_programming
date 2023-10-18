3b2f744793c2d37220bc31ef564488802118e9a2
VandevilleF <flo.vandeville@gmail.com>
2023-10-18 13:22:36 -0700

    Task 9 C - Fonctions, nested loops

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - table multiplication
+ * @n, m: variable
+ * Return: 0
+ */
+void times_table(void)
+{
+	int n, m, t;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (m = 0; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			t = n * m;
+
+			if (t <= 9)
+				_putchar(' ');
+			else
+				_putchar((t / 10) + '0');
+
+			_putchar((t % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
