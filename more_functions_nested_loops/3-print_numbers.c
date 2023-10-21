0887bfcb3a1817f830ff556f92a78b3907192092
VandevilleF <flo.vandeville@gmail.com>
2023-10-21 13:15:14 -0700

    Task 3 C - More functions, more nested loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - affiche nombre
+ * @i: variable
+ * Return 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+	{
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
