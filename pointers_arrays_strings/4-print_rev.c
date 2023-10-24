26db6df660727b5f836115efc712af8f3481cbd7
VandevilleF <flo.vandeville@gmail.com>
2023-10-24 01:59:26 -0700

    Task 4 C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: variable
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (i = i - 1; i >= '\0'; i--)
+			_putchar(s[i]);
+	_putchar('\n');
+}
