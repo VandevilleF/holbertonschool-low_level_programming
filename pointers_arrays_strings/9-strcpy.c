6aebfbca3772cbc7aa165309180145442459cf39
VandevilleF <flo.vandeville@gmail.com>
2023-10-24 13:24:39 -0700

    Task 9 C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcpy - copie string
+ * @dest: variable
+ * @src: variable
+ * Return: copie the string pointed
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+		return (dest);
+}
