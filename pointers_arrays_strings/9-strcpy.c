5460bea132d811c4e0a92317e976e65ef0d21f42
VandevilleF <flo.vandeville@gmail.com>
2023-10-25 01:01:48 -0700

    Update modif totale#1

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,13 +3,21 @@
 #include <stdio.h>
 /**
  * _strcpy - copie string
- * @dest: variable
- * @src: variable
+ * @dest: pointeur, ou la copie est faite
+ * @src: pointeur constant
  * Return: copie the string pointed
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
-		return (dest);
+	int i = 0;
+	/*parcours jusqu'au char nul*/
+	while (src[i] != '\0')
+	{
+	/*marque la fin u string*/
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
