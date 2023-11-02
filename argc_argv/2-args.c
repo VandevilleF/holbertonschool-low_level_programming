d3a5b7c4726213f80644e08a2f1f95cd3e72d9c6
VandevilleF <flo.vandeville@gmail.com>
2023-11-02 14:56:45 -0700

    Task 2 C - argc, argv

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count the number of argument
+ * @argv: 
+ * Return: print all arguments
+ */
+int main( __attribute__((unused)) int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv [i]);
+	return (0);
+}
