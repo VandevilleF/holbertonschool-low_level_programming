0bbf421ed79102ebe5f3b6a8c811aab5df734d67
VandevilleF <flo.vandeville@gmail.com>
2023-11-02 12:18:21 -0700

    Task 0 C - argc, argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: __attribute__((unused))
+ * @argv: array of argument
+ * Return: name of program
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
