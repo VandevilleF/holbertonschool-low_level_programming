13a1970af33a913d77fbbc9b96c6766603cbb321
VandevilleF <flo.vandeville@gmail.com>
2023-11-02 15:03:06 -0700

    Update betty style#1

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -3,14 +3,14 @@
 /**
  * main - main
  * @argc: count the number of argument
- * @argv: 
+ * @argv: display arguments
  * Return: print all arguments
  */
-int main( __attribute__((unused)) int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	printf("%s\n", argv [i]);
+	printf("%s\n", argv[i]);
 	return (0);
 }
