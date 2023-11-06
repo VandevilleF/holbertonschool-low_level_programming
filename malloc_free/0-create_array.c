1de68d8434883771129fba8280d4278d84d235fb
VandevilleF <flo.vandeville@gmail.com>
2023-11-06 06:52:09 -0800

    Update Task 0 with comment

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,13 +11,13 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	array = malloc(size * sizeof(char));
-	if (size == 0)
+	array = malloc(size * sizeof(char));/*Allocate memory for array*/
+	if (size == 0) /*NULL pointer for an empty array*/
 		return (NULL);
 
-	if (array == NULL)
+	if (array == NULL)/*NULL if allocation fails*/
 		return (NULL);
 	for (i = 0; i < size; i++)
-		array[i] = c;
+		array[i] = c;/*Fill array with special char*/
 	return (array);
 }
