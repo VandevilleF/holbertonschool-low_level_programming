6015b601013544c0c22fb46951addf0f49d08861
VandevilleF <flo.vandeville@gmail.com>
2023-11-06 07:43:21 -0800

    Task 1 C - Malloc, free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - copy string
+ * @str: string
+ * Return: copy with success
+ */
+char *_strdup(char *str)
+{
+	char *buffer;
+	unsigned int i;
+
+	buffer = malloc((strlen(str) + 1) * sizeof(str));
+	if (str == 0)
+		return (NULL);
+	if (buffer == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(str) + 1; i++)
+		buffer[i] = str[i];
+	return (buffer);
+}
