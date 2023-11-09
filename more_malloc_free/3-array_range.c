b199a16769fed5e686f457457499a824028d0459
VandevilleF <flo.vandeville@gmail.com>
2023-11-09 05:17:01 -0800

    Update Task 3 commente betty#1

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,13 +14,11 @@ int *array_range(int min, int max)
 	int *array;
 
 	array = malloc((max - min + 1) * 4);
-		if (array == NULL)
-			return (NULL);
+	if (array == NULL)
+		return (NULL);
 	if (min > max)
 		return (NULL);
-	
 	for (i = 0; i <= max - min; i++)
 		array[i] = min + i;
-
 	return (array);
 }
