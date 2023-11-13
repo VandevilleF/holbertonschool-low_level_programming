62e1deaa9cdb2f0d6248ef0f667e887ac2e75316
VandevilleF <flo.vandeville@gmail.com>
2023-11-13 05:26:45 -0800

    Task 2 C - Structures, typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - informe of poppy
+ * @d: pointer
+ * Return: all info of poppy
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d -> name != NULL)
+		printf("%s\n", d -> name);
+	else
+		printf("(nil)\n");
+	printf("Age: %f\n", d -> age);
+	printf("Owner: ");
+	if (d -> owner != NULL)
+		printf("%s\n", d -> owner);
+	else
+		printf("(nil)\n");
+}
