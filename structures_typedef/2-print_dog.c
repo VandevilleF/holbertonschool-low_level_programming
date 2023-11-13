c376a7287288a930462d0e7a16bb10c39ad70bf6
VandevilleF <flo.vandeville@gmail.com>
2023-11-13 05:31:03 -0800

    Update Task 2 betty style#1

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,14 +13,14 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: ");
-	if (d -> name != NULL)
-		printf("%s\n", d -> name);
+	if (d->name != NULL)
+		printf("%s\n", d->name);
 	else
 		printf("(nil)\n");
-	printf("Age: %f\n", d -> age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: ");
-	if (d -> owner != NULL)
-		printf("%s\n", d -> owner);
+	if (d->owner != NULL)
+		printf("%s\n", d->owner);
 	else
 		printf("(nil)\n");
 }
