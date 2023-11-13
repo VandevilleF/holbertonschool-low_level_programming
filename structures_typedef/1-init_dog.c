f789be817c6eec2a68ed5190673d08063b17631a
VandevilleF <flo.vandeville@gmail.com>
2023-11-13 04:37:15 -0800

    Task 0 C - Structures, typedef

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - informe of poppy
+ * @d: pointer
+ * @name : name
+ * @age: age
+ * @owner: onwer
+ * Return: all info of poppy
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef __protect__
+#define __protect__
+/**
+ * struct dog - informe of poppy
+ * @name : name
+ * @age: age
+ * @owner: onwer
+ *
+ * Description: all info of poppy
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	/*Informe of Poppy*/
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif    
