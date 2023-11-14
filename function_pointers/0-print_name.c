6295c5ea98cbbb8e950a585a3f07c74f030cac3b
VandevilleF <flo.vandeville@gmail.com>
2023-11-14 03:29:21 -0800

    Task 0 C - Function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name to print
+ * @f: function pointers
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
