498b3ce930e3f1aa9a2346e45b218d4d1213dd04
VandevilleF <flo.vandeville@gmail.com>
2023-11-28 07:06:34 -0800

    Task 4 C - Singly linked lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ * @head: pointer to pointer
+ * Return: a linked list empty
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		/*Save head in variable temp*/
+		temp = head;
+		/*Make head point to the next node*/
+		head = head->next;
+		/*Safely free variable temp*/
+		free(temp);
+	}
+}
