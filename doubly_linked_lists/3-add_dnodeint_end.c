239d5d08155c1e7ab6f58108f7ed56319c701342
VandevilleF <flo.vandeville@gmail.com>
2023-11-29 02:09:07 -0800

    Task 3 C - Doubly linked lists

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at the end of DLL
+ * @head: pointer to the list
+ * @n: variable
+ * Return: address of the last node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *last_node, *temp;
+
+	last_node = malloc(sizeof(dlistint_t));
+
+	if (last_node == NULL)
+		return (NULL);
+
+	/*Add data in the nex node*/
+	last_node->n = n;
+
+	/*Set pointers of the new node*/
+	last_node->next = NULL;
+	last_node->prev = *head;
+
+	/*Check if list is empty list*/
+	if ((*head) == NULL)
+		(*head) = last_node;
+
+	else
+	{
+		temp = *head;
+
+		/*Traverse to find the last node*/
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		/*Add the node at the end of the DLL*/
+		temp->next = last_node;
+	}
+	return (last_node);
+}
