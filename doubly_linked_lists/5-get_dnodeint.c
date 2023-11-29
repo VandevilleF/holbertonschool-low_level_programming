a590f771bbc3e8330859802fd7e8201e3c35b1f5
VandevilleF <flo.vandeville@gmail.com>
2023-11-29 05:33:17 -0800

    Task 5 C - Doubly linked lists

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - print value of a n node
+ * @head: pointer to DLL
+ * @index: element give for find a node in the DLL
+ * Return: value of the node find with index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int find = 0;
+
+	while (find != index)
+	{
+		head = head->next;
+		find++;
+	}
+	return (head);
+}
