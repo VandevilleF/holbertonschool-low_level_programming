b51bf61c26c69c27a1fce63ef9392042f5ceef68
VandevilleF <flo.vandeville@gmail.com>
2023-12-04 07:09:20 -0800

    Add comment for code

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,24 +12,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t file_d, file_r, file_w;
 	char *buffer;
 
+	/*Allocate memory for the buffer*/
 	buffer = malloc(letters);
 	if (buffer == NULL)
 		return (0);
 
+	/*Check if filename is NULL*/
 	if (filename == NULL)
 		return (0);
 
+	/*Open filename for read only*/
 	file_d = open(filename, O_RDONLY);
 
+	/*Check if opened is not fail*/
 	if (file_d == -1)
 	{
 		free(buffer);
 		return (0);
 	}
+	/*Read data from file*/
 	file_r = read(file_d, buffer, letters);
 
+	/*Write the read content to stdout*/
 	file_w = write(STDOUT_FILENO, buffer, file_r);
 
+	/*Close file*/
 	close(file_d);
 
 	return (file_w);
