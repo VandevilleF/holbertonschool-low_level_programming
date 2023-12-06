ef66eddef0db8cc9392442a254b90e6dd21a3f26
VandevilleF <flo.vandeville@gmail.com>
2023-12-06 05:17:04 -0800

    Add comments for the code

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *newtab;
 
+	/*Allocate memory for array*/
 	newtab = malloc(sizeof(hash_table_t));
 
 	if (newtab == NULL)
@@ -18,6 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	newtab->size = size;
 
+	/*Allocate memory for an array of hash_node_t pointers in the newtab struct*/
 	newtab->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (newtab->array == NULL)
@@ -25,6 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(newtab);
 		return (NULL);
 	}
+	/*Set all value on array to NULL*/
 	for (i = 0; i < size; i++)
 		newtab->array[i] = NULL;
 
